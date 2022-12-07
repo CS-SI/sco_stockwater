d845f4399a7668d871c245b8eb2a8381eae10c46
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2022-12-07 15:16:29 +0100

    chore: set base url

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,20 @@ import geojson from 'rollup-plugin-geojson'
 const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		geojson({
-			compact: true,
-		}),
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@components': path.resolve(__dirname, './src/components'),
-			'@data': path.resolve(__dirname, './src/data'),
-			'@stores': path.resolve(__dirname, './src/stores'),
-			'@layers': path.resolve(__dirname, './src/layers'),
-		},
-	},
+  plugins: [
+    react(),
+    geojson({
+      compact: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@stores': path.resolve(__dirname, './src/stores'),
+      '@layers': path.resolve(__dirname, './src/layers')
+    }
+  },
+  base: '/'
 })
